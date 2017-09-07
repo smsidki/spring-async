e575a60f30dc3a12efc578df09039d0cbc931f95
smsidki <sichy_helsinky@yahoo.com>
2017-09-07 09:31:42 +0700

    change sysout to logger.

--- src/main/java/me/springframework/async/config/error/AsyncExceptionHandler.java
+++ src/main/java/me/springframework/async/config/error/AsyncExceptionHandler.java
@@ -2,16 +2,20 @@ package me.springframework.async.config.error;
 
 import java.lang.reflect.Method;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 
 public class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(AsyncExceptionHandler.class);
 
 	@Override
 	public void handleUncaughtException(Throwable ex, Method method, Object... params) {
-		System.out.println("Exception message - " + ex.getMessage());
-		System.out.println("Method name - " + method.getName());
+		LOG.error("Exception message - " + ex.getMessage());
+		LOG.error("Method name - " + method.getName());
 		for (Object param : params) {
-			System.out.println("Parameter value - " + param);
+			LOG.error("Parameter value - " + param);
 		}
 	}
 
